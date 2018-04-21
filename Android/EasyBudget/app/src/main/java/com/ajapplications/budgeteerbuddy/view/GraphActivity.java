3a03eb992cb7cdc3624579e32b1a7dd587708ac8
Jordan Judt <dev@jordanjudt.com>
2018-04-20 23:13:16 -0500

    Copy onOptionsItemSelected

--- Android/EasyBudget/app/src/main/java/com/ajapplications/budgeteerbuddy/view/GraphActivity.java
+++ Android/EasyBudget/app/src/main/java/com/ajapplications/budgeteerbuddy/view/GraphActivity.java
@@ -2,6 +2,7 @@ package com.ajapplications.budgeteerbuddy.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ajapplications.budgeteerbuddy.R;
 
@@ -14,5 +15,19 @@ public class GraphActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if( id == android.R.id.home ) // Back button of the actionbar
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
